8c0f636689a0d62d5ac51acc6662dd17b925efce
“donpolo” <poli123hj@gmail.com>
2022-11-15 21:24:32 -0500

    CRUD Instructores

--- programa/urls.py
+++ programa/urls.py
@@ -5,10 +5,12 @@ urlpatterns = [
     path('programa/', views.mostrar_programa_formacion, name='programa'),
     path('programa/create/', views.create_Programa, name='createPrograma'),
     path('editar/<int:programa_id>/', views.editarPrograma, name='editarPrograma'),
-        path('eliminar/<int:programa_id>/', views.eliminarPrograma, name='eliminarPrograma'),
+    path('eliminar/<int:programa_id>/', views.eliminarPrograma, name='eliminarPrograma'),
     path('filtrar/', views.filtrar_programa_formacion, name='filtro'),
- 
+    path('instructores/', views.mostrar_Instructores, name='instructores'),
+    path('instructores/create/', views.create_Instructor, name='createInstructor'),
+    path('editar/instructor/<int:instructor_id>/', views.editarInstructor, name='editarInstructor'),
+    path('eliminar/instructor/<int:instructor_id>/', views.eliminarInstructor, name='eliminarInstructor'),
 ]
 
-   # path('trimestre/<int:programa_id>/', views.programa_formacion, name='programa'),
-    # path('', views.home, name='home'),
+  
